f1bc24ee9390054fd678c5e29496c8769dc1ab13
Crowdbotics <engineering@crowdbotics.com>
2024-01-16 15:29:27 +0000

    Initial commit

---
+++ backend/hithi_dev_132587/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for hithi_dev_132587 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "hithi_dev_132587.settings"
+)
+
+application = get_wsgi_application()
